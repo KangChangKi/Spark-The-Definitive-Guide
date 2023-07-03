293518b6a7d81dc43dd5746b151b0d75196e214b
KangChangKi <changki.kang@gmail.com>
2023-07-03 09:13:15 +0900

    실행시키기 위한 코드 변경.

--- code/Ecosystem-Chapter_32_Language_Specifics.py
+++ code/Ecosystem-Chapter_32_Language_Specifics.py
@@ -1,17 +1,14 @@
+from pyspark.sql import SparkSession
+
+spark = (SparkSession.builder.appName("test1").master("local")
+  .getOrCreate())
+
+path_prefix = "file:///Users/lenani/SynologyDrive/test_personal/Spark-The-Definitive-Guide"
+
 import pandas as pd
 df = pd.DataFrame({"first":range(200), "second":range(50,250)})
 
-
-# COMMAND ----------
-
 sparkDF = spark.createDataFrame(df)
-
-
-# COMMAND ----------
 
 newPDF = sparkDF.toPandas()
 newPDF.head()
-
-
-# COMMAND ----------
-
